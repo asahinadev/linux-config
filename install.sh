16b1ac83ec9a3dd7ce4d14b4eb487c212ed95e60
asahinadev <59528169+asahinadev@users.noreply.github.com>
2021-05-10 17:04:40 +0900

    change localized install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ MAIL=$1
 # ドメイン設定
 DOMAIN=$2
 
+RAW_URL=https://raw.githubusercontent.com/asahinadev/linux-config/main
+
 if [ "$1" = "" ]; then
   echo "Usage $0 MAIL DOMAIN" >&2
   echo "MIAL は必須です。" >&2
@@ -35,9 +37,7 @@ firewall-cmd --permanent \
     --add-service=http   \
     --add-service=https  \
     --add-service=imaps  \
-    --add-service=smtps  \
-    --add-service=imap   \
-    --add-service=smtp   \
+    --add-service=smtps  
 
 #################
 # nginx 設定    #
@@ -50,12 +50,12 @@ firewall-cmd --list-all --permanent
 
 cd /etc/nginx/
 cp -a nginx.conf nginx.conf.org
-curl -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/nginx/nginx.conf
+curl -O $RAW_URL/etc/nginx/nginx.conf
 
 cd conf.d
 curl \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/nginx/cd%20conf.d/00.default.server.conf \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/nginx/cd%20conf.d/01.www.server.conf
+ -O $RAW_URL/etc/nginx/conf.d/00.default.server.conf \
+ -O $RAW_URL/etc/nginx/conf.d/01.www.server.conf
 sed -i "s/__DOMAIN__/$DOMAIN/g" 00.default.server.conf
 sed -i "s/__DOMAIN__/$DOMAIN/g" 01.www.server.conf
 
@@ -65,43 +65,43 @@ certbot certonly \
     --non-interactive           \
     --keep                      \
     --expand                    \
-    -d mirror-world.work        \
-    -d www.mirror-world.work    \
-    -d test.mirror-world.work   \
-    -d gitlab.mirror-world.work \
-    -d mail.mirror-world.work   \
-    --email $MAIL \
-  2>&1 | tee -a  $LOG.stdout
+    -d $DOMAIN                  \
+    -d www.$DOMAIN              \
+    -d test.$DOMAIN             \
+    -d gitlab.$DOMAIN           \
+    -d mail.$DOMAIN             \
+    --email $MAIL
 
 cd /etc/postfix/
 cp -a main.cf   main.cf.org
 cp -a master.cf master.cf.org
 curl \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/postfix/main.cf \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/postfix/master.cf
+ -O $RAW_URL/etc/postfix/main.cf \
+ -O $RAW_URL/etc/postfix/master.cf
+
 # ドメイン調整
 sed -i "s/__DOMAIN__/$DOMAIN/g" main.cf
 cat main.cf | grep $DOMAIN
 
 cd /etc/dovecot
 cp -a dovecot.conf dovecot.conf.org
-curl -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/dovecot/dovecot.conf
+
+curl -O $RAW_URL/etc/dovecot/dovecot.conf
 
 cd conf.d
 cp -a 10-master.conf 10-master.conf.org
 cp -a 10-ssl.conf    10-ssl.conf.org
 cp -a 10-auth.conf   10-auth.conf.org
 cp -a 10-mail.conf   10-mail.conf.org
+
 curl \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/dovecot/conf.d/10-auth.conf \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/dovecot/conf.d/10-mail.conf \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/dovecot/conf.d/10-master.conf \
- -O https://raw.githubusercontent.com/asahinadev/linux-config/main/etc/dovecot/conf.d/10-ssl.conf
-sed -i "s/__DOMAIN__/mirror-world.work/g" 10-ssl.conf
+ -O $RAW_URL/etc/dovecot/conf.d/10-auth.conf \
+ -O $RAW_URL/etc/dovecot/conf.d/10-mail.conf \
+ -O $RAW_URL/etc/dovecot/conf.d/10-master.conf \
+ -O $RAW_URL/etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/__DOMAIN__/$DOMAIN/g" 10-ssl.conf
 cat 10-ssl.conf | grep $DOMAIN
 
-
-
-systemctl enable postfix dovecot saslauthd
-systemctl start  postfix dovecot saslauthd
+systemctl enable postfix dovecot
+systemctl start  postfix dovecot
 
